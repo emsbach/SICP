03bcf4ef18c33fb8348ac397a08376f928a8cdb6
emsbach <38355647+emsbach@users.noreply.github.com>
2019-01-05 13:04:37 +0100

    Power set

---
+++ Exercise 2.32.rkt
@@ -0,0 +1,12 @@
+#lang sicp
+
+(define (subsets s)
+  (if (null? s)
+      (list nil)
+      (let ((rest (subsets (cdr s))))
+        (append rest (map (lambda (x) (cons (car s) x))
+                          rest)))))
+
+; Testing
+(define s (list 1 2 3))
+(display (subsets s))
